78a04c418ff5098e023f1effd073acc978ea59e1
Marc <marc.ferraro1@gmail.com>
2021-02-10 16:17:37 -0600

    commit tree 70aa0796b0c5fd34777a2e8a636423640426e068

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const superbowlWin = (array) => {
+  const win = array.find(record => record.result === "W")
+  // console.log(test.year)
+  if (win !== undefined){
+    return win.year
+  } else {
+    win
+  }
 }
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ] 
+// superbowlWin(record)
